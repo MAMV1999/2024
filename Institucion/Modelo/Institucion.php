458f8b01a275cd742af84c88162e63f4fe50da43
MAMV1999 <mmanriquevarillas25@gmail.com>
2024-11-13 17:12:44 -0500

    131124_1

--- Institucion/Modelo/Institucion.php
+++ Institucion/Modelo/Institucion.php
@@ -10,17 +10,14 @@ class Institucion
     // Método para guardar una nueva institución
     public function guardar($nombre, $id_trabajador, $telefono, $correo, $direccion)
     {
-        $sql = "INSERT INTO institucion (nombre, id_trabajador, telefono, correo, direccion) 
-                VALUES ('$nombre', '$id_trabajador', '$telefono', '$correo', '$direccion')";
+        $sql = "INSERT INTO institucion (nombre, id_trabajador, telefono, correo, direccion) VALUES ('$nombre', '$id_trabajador', '$telefono', '$correo', '$direccion')";
         return ejecutarConsulta($sql);
     }
 
     // Método para editar una institución existente
     public function editar($id, $nombre, $id_trabajador, $telefono, $correo, $direccion)
     {
-        $sql = "UPDATE institucion 
-                SET nombre='$nombre', id_trabajador='$id_trabajador', telefono='$telefono', correo='$correo', direccion='$direccion' 
-                WHERE id='$id'";
+        $sql = "UPDATE institucion SET nombre='$nombre', id_trabajador='$id_trabajador', telefono='$telefono', correo='$correo', direccion='$direccion' WHERE id='$id'";
         return ejecutarConsulta($sql);
     }
 
@@ -34,9 +31,7 @@ class Institucion
     // Método para listar todas las instituciones
     public function listar()
     {
-        $sql = "SELECT i.id, i.nombre, t.nombre_apellido AS trabajador, i.telefono, i.correo, i.direccion, i.estado
-                FROM institucion i
-                LEFT JOIN trabajador t ON i.id_trabajador = t.id";
+        $sql = "SELECT i.id, i.nombre, t.nombre_apellido AS trabajador, i.telefono, i.correo, i.direccion, i.estado FROM institucion i LEFT JOIN trabajador t ON i.id_trabajador = t.id";
         return ejecutarConsulta($sql);
     }
 
@@ -57,10 +52,7 @@ class Institucion
     // Método para listar los directores activos que no están asignados a una institución activa
     public function listarDirectoresActivos()
     {
-        $sql = "SELECT t.id, t.nombre_apellido 
-                FROM trabajador t 
-                LEFT JOIN institucion i ON t.id = i.id_trabajador AND i.estado = '1'
-                WHERE t.cargo='DIRECTOR' AND t.estado='1' AND i.id_trabajador IS NULL";
+        $sql = "SELECT t.id, t.nombre_apellido FROM trabajador t LEFT JOIN institucion i ON t.id = i.id_trabajador AND i.estado = '1' WHERE t.cargo='DIRECTOR' AND t.estado='1' AND i.id_trabajador IS NULL";
         return ejecutarConsulta($sql);
     }
 }
